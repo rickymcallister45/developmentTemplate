be3e2261480c584b280c55dca4873ce3185df75e
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-25 05:39:07 -0400

    Create head.php

---
+++ php/head.php
@@ -0,0 +1,17 @@
+<?php
+echo
+"<!DOCTYPE HTML>
+
+<html>
+  <head>";
+    
+    include "./metas.php";
+  
+  echo  "<link rel='stylesheet' href='css/zeroOutStyle.css'>
+         <link rel='stylesheet' href='css/style.css'>
+  </head> ";
+
+
+
+
+?>
